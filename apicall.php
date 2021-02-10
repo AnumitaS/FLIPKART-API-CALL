690f1a26c274dc88782c817f565bd7e2f13a1ee7
AnumitaS <41711532+AnumitaS@users.noreply.github.com>
2021-02-10 13:31:31 +0530

    Create apicall.php

---
+++ apicall.php
@@ -0,0 +1,46 @@
+<?php
+# This file will fetch data from flipkart affiliate API 
+#Author: Anumita Singha Roy.
+#website: https://www.indoxer.com
+#License: You can use , modify, distribute this code. But do not change the author name. You can apply your name at your edited lines.
+
+// creating api calling function
+
+error_reporting(0); // By this line we are hiding all errors, if you want to see errors, then put a # at the beginning of this line.
+function callAPI($method, $url, $data)
+{
+   $curl = curl_init();
+   switch ($method){
+      case "POST":
+         curl_setopt($curl, CURLOPT_POST, 1);
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+         break;
+      case "PUT":
+         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+         if ($data)
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+         break;
+      default:
+         if ($data)
+            $url = sprintf("%s?%s", $url, http_build_query($data));
+   }
+   // OPTIONS:
+   curl_setopt($curl, CURLOPT_URL, $url);
+   curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+      'APIKEY: 2a983119bdb241c99be08de6aa60cf92',
+      'Content-Type: application/json',
+	  'Fk-Affiliate-Id:samzonedis',
+	  'Fk-Affiliate-Token:2a983119bdb241c99be08de6aa60cf92'
+	  
+   ));
+   curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+   curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+   // EXECUTE:
+   $result = curl_exec($curl);
+   if(!$result){die("Connection Failure");}
+   curl_close($curl);
+   return $result;
+}
+
+?>
